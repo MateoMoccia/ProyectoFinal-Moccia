55ec0d69ac6998bd76dab345c3197e489328a85b
Mateo Moccia <mateomoccia@gmail.com>
2025-02-20 23:02:15 -0300

    Cuarto commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tienda-de-carteras/', // Asegúrate de que este es el nombre correcto del repo en GitHub
+  base: '/ProyectoFinal-Moccia/',
 })
