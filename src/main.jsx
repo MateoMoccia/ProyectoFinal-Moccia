1259c7a7b2383a466d9eacf88704023372a1ef99
Mateo Moccia <mateomoccia@gmail.com>
2025-02-21 00:21:05 -0300

    Doceavo commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; // Cambié la importación
 import { StrictMode } from 'react';
-import App from './App';
-import { CarritoProvider } from './Context/CarritoContext';
+import App from '/ProyectoFinal-Moccia/src/App';
+import { CarritoProvider } from '/ProyectoFinal-Moccia/src/Context/CarritoContext';
 
 // Usamos createRoot en lugar de render
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ProyectoFinal-Moccia/',
-})
+  base: '/ProyectoFinal-Moccia/',  // Esto asegura que las rutas sean correctas para GitHub Pages
+});
