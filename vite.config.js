da319a3b88ef373c31ba58c57ec639ed69521766
Mateo Moccia <mateomoccia@gmail.com>
2025-02-21 00:12:06 -0300

    Decimo commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Cambié el base a '/' para que las rutas funcionen bien localmente
+  base: '/ProyectoFinal-Moccia/',
 })
