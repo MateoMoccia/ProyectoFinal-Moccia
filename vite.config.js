fa07ad1c9a4a5b90a8f8e5864674d7adcc28a2e4
Mateo Moccia <mateomoccia@gmail.com>
2025-02-21 00:36:21 -0300

    Treceavo commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ProyectoFinal-Moccia/',  // Esto asegura que las rutas sean correctas para GitHub Pages
+  base: '/ProyectoFinal-Moccia/',
+  server: {
+    mimeTypes: {
+      jsx: 'application/javascript',
+    }
+  },
 });
