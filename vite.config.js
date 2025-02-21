6664dd8d6e43d8bc120905de1c5e107d1588871c
Mateo Moccia <mateomoccia@gmail.com>
2025-02-21 00:50:04 -0300

    Catorceavo commit

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ProyectoFinal-Moccia/',
-  server: {
-    mimeTypes: {
-      jsx: 'application/javascript',
-    }
-  },
+  base: '/ProyectoFinal-Moccia/',  // Asegura que las rutas sean correctas
 });
